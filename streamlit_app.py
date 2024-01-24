05fb56904d612fa14cd19c349e5af76e2f1573e1
ahmedalhammadi <aaq1806@gmail.com>
2024-01-24 22:38:34 +0400

    New update

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,18 @@ from openai import OpenAI
 
 # Initialize the OpenAI client with your API key
 client = OpenAI(api_key=st.secrets["openai_api_key"])
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 # Function to communicate with ChatGPT
 def chat_with_gpt(prompt):
     # Create a chat completion
